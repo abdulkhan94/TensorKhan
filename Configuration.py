fcee2f71554e52503aa72cbf626f10ebe64dfe0a
Abdullah Khan <abdullahn@gmail.com>
2020-08-05 00:14:51 -0400

    Env check

--- Configuration.py
+++ Configuration.py
@@ -1,8 +1,8 @@
 import silence_tensorflow.auto
 import tensorflow as tf
 
-x1 = tf.constant([1, 2, 3, 4])
-x2 = tf.constant([5, 6, 7, 8])
+x1 = tf.constant([1, 2, 3, 4, 9])
+x2 = tf.constant([5, 6, 7, 8, 1])
 
 result = tf.multiply(x1, x2)
 
